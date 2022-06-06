ca4f695057140ccd8d3c031caa2cfd2c6a27eb98
Renato Ceolin <renato.ceolin@renatoceolin.com>
2022-06-06 16:24:17 -0300

    Initial commit (mix new)

---
+++ lib/self_rehydrating_cache.ex
@@ -0,0 +1,18 @@
+defmodule SelfRehydratingCache do
+  @moduledoc """
+  Documentation for `SelfRehydratingCache`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SelfRehydratingCache.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/self_rehydrating_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule SelfRehydratingCacheTest do
+  use ExUnit.Case
+  doctest SelfRehydratingCache
+
+  test "greets the world" do
+    assert SelfRehydratingCache.hello() == :world
+  end
+end
